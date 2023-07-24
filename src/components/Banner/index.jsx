5ba7e6bc029d4c7a57e1cae744bffa3c3e2455e9
Valentin <Valent.bene@gmail.com>
2023-07-24 20:21:00 +0200

    About banner height

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,10 +1,18 @@
 //COMPOSANT BANNIERE (PAGE D'ACCUEIL ET PAGE A PROPOS)
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import '../../utils/scss/Components/_BannerStyles.scss'
 
 const Banner = ({ image, children }) => {
+  const location = useLocation() //useLocation permet de vérifier l'url actif
+  const isActive = (pathname) => {
+    return location.pathname === pathname ? true : false
+  }
   return (
-    <div className="banner" style={{ '--banner-image': `url(${image})` }}>
+    <div
+      className={`banner ${isActive('/about') ? 'banner--about' : ''}`}
+      style={{ '--banner-image': `url(${image})` }}
+    >
       {children}
     </div>
   )
